0e374d7d79def45129c2fa1409e6c6e884b4cb31
txzy2 <txzy2@yandex.ru>
2026-07-29 19:38:57 +0300

    добавил руну и  пофиксил рецепты

--- src/main/java/com/voidenergy/item/ModItems.java
+++ src/main/java/com/voidenergy/item/ModItems.java
@@ -14,11 +14,9 @@ import net.minecraft.world.item.ItemStack;
 
 public class ModItems {
     // Регистрируем предмет
-    public static final Item MAGIC_STICK = new MagicStick(
-            new Item.Properties().durability(MagicStick.DURABILITY));
-
-    public static final Item VOID = new Void(
-            new Item.Properties().durability(256));
+    public static final Item MAGIC_STICK = new MagicStick(new Item.Properties().durability(MagicStick.DURABILITY));
+    public static final Item VOID = new Void(new Item.Properties().durability(256));
+    public static final Item VOID_RUNE = new Item(new Item.Properties());
 
     // Метод register() для вызова из ExampleMod
     public static void register() {
@@ -32,9 +30,15 @@ public class ModItems {
                 ResourceLocation.fromNamespaceAndPath(ExampleMod.MOD_ID, "void"),
                 VOID);
 
+        Registry.register(
+                BuiltInRegistries.ITEM,
+                ResourceLocation.fromNamespaceAndPath(ExampleMod.MOD_ID, "void_rune"),
+                VOID_RUNE);
+
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> {
             entries.accept(new ItemStack(MAGIC_STICK));
             entries.accept(new ItemStack(VOID));
+            entries.accept(new ItemStack(VOID_RUNE));
         });
     }
 
